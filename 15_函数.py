04b48f3149dd6d9d906e1ecea1b0ee03129b3e8b
Mo-zj <809366803@qq.com>
2023-09-11 23:16:13 +0800

    TR-2309112316

--- 15_函数.py
+++ 15_函数.py
@@ -151,10 +151,36 @@ def f(a, b, *, c):
     return a+b+c
 
 
+# todo：匿名函数
+'''
+Python 使用 lambda 来创建匿名函数。
+所谓匿名，意即不再使用 def 语句这样标准的形式定义一个函数。
+   lambda 只是一个表达式，函数体比 def 简单很多。
+   lambda 的主体是一个表达式，而不是一个代码块。仅仅能在 lambda 表达式中封装有限的逻辑进去。
+   lambda 函数拥有自己的命名空间，且不能访问自己参数列表之外或全局命名空间里的参数。
+   虽然 lambda 函数看起来只能写一行，却不等同于 C 或 C++ 的内联函数，内联函数的目的是调用小函数时不占用栈内存从而减少函数调用的开销，提高代码的执行速度。
 
+语法：
+lambda 函数的语法只包含一个语句，如下：
+lambda [arg1 [,arg2,.....argn]]:expression
+'''
+def NiMingHanShu():
+    # 设置参数 a 加上 10:
+    x = lambda a : a+10
+    print(x(5))      # 输出：15
 
+    # 可写函数说明
+    sum = lambda arg1, arg2 : arg1 + arg2
+    # 调用sum函数
+    print("相加后的值为：", sum(10, 20))      # 输出：30
+    print("相加后的值为：", sum(20, 20))      # 输出：40
 
-
+'''
+我们可以将匿名函数封装在一个函数内，这样可以使用同样的代码来创建多个匿名函数。
+以下实例将匿名函数封装在 myfunc 函数中，通过传入不同的参数来创建不同的匿名函数：
+'''
+def myfunc(n):
+    return lambda a : a * n
 
 
 
@@ -234,12 +260,32 @@ if __name__ == '__main__':
     # -------------------------------
     # todo：不定长参数，如果单独出现星号 *，则星号 * 后的参数必须用关键字传入：def f(a, b, *, c):
     # 报错
-    try:
-        f(1, 2, 3)
-    except TypeError:
-        print("TypeError: f() takes 2 positional arguments but 3 were given")
+    # try:
+    #     f(1, 2, 3)
+    # except TypeError:
+    #     print("TypeError: f() takes 2 positional arguments but 3 were given")
+    #
+    # # 正常
+    # print(f(1, 2, c=3))      # 输出：6
+    # -------------------------------
 
-    # 正常
-    print(f(1, 2, c=3))      # 输出：6
+    # -------------------------------
+    # todo：匿名函数
+    NiMingHanShu()
+
+    # 匿名函数封装在 myfunc 函数中，通过传入不同的参数来创建不同的匿名函数
+    mydoubler = myfunc(2)
+    mytripler = myfunc(3)
+
+    print(mydoubler(11))      # 输出：22
+    print(mytripler(11))      # 输出：33
+
+
+
+
+
+
+
+
 
 
